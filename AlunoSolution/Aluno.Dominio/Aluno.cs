1712031a3f9996b241208aa6b5979dbe9cbea6fc
Gabriel Vidal <gabriel77723@gmail.com>
2025-08-29 21:20:46 -0300

    introduçao aula2

---
+++ AlunoSolution/Aluno.Dominio/Aluno.cs
@@ -0,0 +1,15 @@
+﻿namespace Aluno.Dominio
+{
+    public class Aluno
+    {
+         public string Nome { get; set;  }
+        public int Id { get; set;   } 
+        public int Idade { get; set; }
+
+        public bool EhMaiordeIdade() 
+        {
+            return Idade >= 18;
+        }
+        
+    }
+}
